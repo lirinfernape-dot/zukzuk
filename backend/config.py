2d694c31071e300ad6cdc71c8d5859ccc9a91c36
lirinfernape-dot <lirinfernape@gmail.com>
2026-07-18 23:26:00 -0500

    Corregido config.py - import os al inicio

--- backend/config.py
+++ backend/config.py
@@ -18,24 +18,4 @@ UPLOAD_PORTADAS = os.path.join(UPLOADS, "portadas")
 UPLOAD_JUEGOS = os.path.join(UPLOADS, "juegos")
 
 # Configuración para Render
-RENDER = os.environ.get('RENDER', False)import os
-
-# Carpeta del backend
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# Configuración del servidor
-HOST = "0.0.0.0"
-PORT = 10000
-DEBUG = False
-
-# JWT
-SECRET_KEY = "kayxor10"
-
-# Carpetas de subida
-UPLOADS = os.path.join(BASE_DIR, "uploads")
-UPLOAD_AVATARS = os.path.join(UPLOADS, "avatars")
-UPLOAD_PORTADAS = os.path.join(UPLOADS, "portadas")
-UPLOAD_JUEGOS = os.path.join(UPLOADS, "juegos")
-
-# Configuración para Render
 RENDER = os.environ.get('RENDER', False)
